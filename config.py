3c45b0ef3e559a4ff474a8801c5e89edb7c411bb
BraveCactus <mattdav961@gmail.com>
2026-07-16 16:27:53 +0700

    fix .gitignore and config.py: solve folder's names conflict

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     data_dir: Path = Path("dataset")
-    models_dir: Path = Path("models")
+    models_dir: Path = Path("models-storage")
     output_dir: Path = Path("output")
 
 config = Config()
